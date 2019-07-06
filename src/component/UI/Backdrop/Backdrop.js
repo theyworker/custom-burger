39213108a507b26b326ddd85cef90164f614bea3
Devaka <devaka.kumarasinghe@gmail.com>
2019-07-06 19:47:06 +0530

    Modal Closing

---
+++ src/component/UI/Backdrop/Backdrop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Backdrop.css'
+
+const Backdrop = (props) => {
+    if (props.show) {
+        return <div className={classes.Backdrop}
+            onClick={props.modalclicked}>
+        </div>
+    }
+    else {
+        return null;
+    }
+
+}
+
+export default Backdrop;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true})
     }
 
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing : false})
+    }
+
 
     render() {
         const disabledInfo = { ...this.state.ingredients };
@@ -82,7 +86,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalclick={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
